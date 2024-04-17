625ea7204d52d503ba6818378b02cf0c57d36f99
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-04-17 15:58:32 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Defer it 🚦
+
+This is a simple serverless function that defers a request for a given amount of time.
+
+## Usage
+
+```bash
+curl https://defer-it.vercel.app?waitFor=3000&url=https://example.com
+```
---
+++ api/index.js
@@ -0,0 +1,46 @@
+const Fastify = require("fastify");
+const { FormatRegistry, Type } = require("@sinclair/typebox");
+const { TypeBoxValidatorCompiler } = require("@fastify/type-provider-typebox");
+const isURL = require("is-url");
+
+// Utils
+
+FormatRegistry.Set("url", isURL);
+
+const QuerySchema = Type.Object({
+  url: Type.String({ format: "url" }),
+  waitFor: Type.Optional(Type.Integer({ default: 1000 })),
+});
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Fastify app
+
+const app = Fastify({
+  logger: true,
+}).setValidatorCompiler(TypeBoxValidatorCompiler);
+
+app.get(
+  "/",
+  {
+    schema: {
+      querystring: QuerySchema,
+    },
+  },
+  async (request, reply) => {
+    const { url, waitFor } = request.query;
+
+    await sleep(waitFor);
+
+    reply.redirect(url);
+  }
+);
+
+// Serverless handler
+
+async function handler(req, reply) {
+  await app.ready();
+  app.server.emit("request", req, reply);
+}
+
+module.exports = handler;
